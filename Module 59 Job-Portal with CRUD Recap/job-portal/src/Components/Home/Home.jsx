3a067423e7873b905e58547995eeda7603bd90fa
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2025-11-22 02:54:47 +0600

    59-2 Add Jobs Data and Load data in the client Side

--- Module 59 Job-Portal with CRUD Recap/job-portal/src/Components/Home/Home.jsx
+++ Module 59 Job-Portal with CRUD Recap/job-portal/src/Components/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
+import Hotjobs from './Hotjobs';
 
 const Home = () => {
+
+    const jobsPromise = fetch('http://localhost:3000/jobs').then(res => res.json())
     return (
         <div>
             <Banner></Banner>
+            <Hotjobs jobsPromise={jobsPromise}></Hotjobs>
         </div>
     );
 };
