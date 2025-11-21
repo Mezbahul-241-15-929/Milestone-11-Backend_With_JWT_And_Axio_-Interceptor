c61b6af50202b188fea70d4da5c17f54a6656613
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2025-11-22 03:20:09 +0600

    59-3 Display Job data using Job Card

--- Module 59 Job-Portal with CRUD Recap/job-portal/src/Components/Home/Home.jsx
+++ Module 59 Job-Portal with CRUD Recap/job-portal/src/Components/Home/Home.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Banner from './Banner';
-import Hotjobs from './Hotjobs';
+import Hotjobs from '../Jobs/Hotjobs';
 
 const Home = () => {
 
-    const jobsPromise = fetch('http://localhost:3000/jobs').then(res => res.json())
     return (
         <div>
             <Banner></Banner>
-            <Hotjobs jobsPromise={jobsPromise}></Hotjobs>
+            <Hotjobs></Hotjobs>
         </div>
     );
 };
