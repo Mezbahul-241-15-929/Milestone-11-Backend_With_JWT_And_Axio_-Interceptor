79d76c2db1f5d4445622d0b78e9ae2ffacfe63cd
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2025-11-28 03:32:21 +0600

    59-9 Show my Application in Table

--- Module 59 Job-Portal with CRUD Recap/job-portal/src/Components/Jobs/Myapplications.jsx
+++ Module 59 Job-Portal with CRUD Recap/job-portal/src/Components/Jobs/Myapplications.jsx
@@ -1,19 +1,25 @@
 import React, { Suspense } from 'react';
 import ApplicationsStats from './ApplicationsStats';
 import useAuth from '../../hooks/useAuth';
+import Applicationlist from './Applicationlist';
+import { myApplicationsPromise } from '../../api/application';
 
-const myApplicationsPromise =email=>{
-    return fetch(`http://localhost:3000/applications?email=${email}`)
-    .then(res=>res.json());
-}
+// export const myApplicationsPromise = email => {
+//     return fetch(`http://localhost:3000/applications?email=${email}`).then(res => res.json());
+// }
 
 const Myapplications = () => {
 
-    const {user} = useAuth();
+    const { user } = useAuth();
     return (
         <div>
             <ApplicationsStats></ApplicationsStats>
-            <Suspense></Suspense>
+            <Suspense fallback={'Loading application'}>
+                <Applicationlist
+                myApplicationsPromise={myApplicationsPromise(user.email)}
+                >    
+                </Applicationlist>
+            </Suspense>
         </div>
     );
 };
