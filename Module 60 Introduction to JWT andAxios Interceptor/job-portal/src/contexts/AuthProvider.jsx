35ea258e41fc9ea85f23b7d12bbe6fbd0b232bb5
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2025-12-02 23:35:40 +0600

    60-3 jwt token

--- Module 60 Introduction to JWT andAxios Interceptor/job-portal/src/contexts/AuthProvider.jsx
+++ Module 60 Introduction to JWT andAxios Interceptor/job-portal/src/contexts/AuthProvider.jsx
@@ -7,7 +7,7 @@ import axios from 'axios';
 const googleProvider = new GoogleAuthProvider();
 
 
-
+//AuthProvider
 const AuthProvider = ({ children }) => {
 
     const [loading ,setLoading]= useState(true);
