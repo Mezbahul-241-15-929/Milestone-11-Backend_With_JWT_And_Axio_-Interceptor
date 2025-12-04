c0c65b9f60702cb9033d14824a60e8bebb4ff739
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2025-12-04 15:02:05 +0600

    60-8 module summary

--- Module 60 Introduction to JWT andAxios Interceptor/job-portal/src/api/jobsapi.js
+++ Module 60 Introduction to JWT andAxios Interceptor/job-portal/src/api/jobsapi.js
@@ -1,5 +1,7 @@
 export const jobsCreatedByPromise =email=>{
     // return fetch(`http://localhost:3000/jobs?email=${email}`)
-    return fetch(`http://localhost:3000/jobs/applications?email=${email}`)
+    return fetch(`http://localhost:3000/jobs/applications?email=${email}`,{
+        credentials: 'include'
+    })
     .then(res=>res.json());
 }
